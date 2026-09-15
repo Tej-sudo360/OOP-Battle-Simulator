6bb9d7d311c234ddd0368e6d2aa61ff5e6d52e10
Tej-sudo360 <tej@akash.com>
2026-09-15 14:16:41 -0500

    sum cool wha done
    I did somthing cool!

--- game.py
+++ game.py
@@ -1,25 +1,43 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "The Streets of Demopolis"
+
+def battle(hero: Hero,enemy: Goblin):
+    while hero.is_alive and enemy.is_alive():
+        hero_damage = hero.attack()
+        enemy.take_damage(hero_damage)
+
+        if enemy.is_alive():
+            enemy_damage = enemy.attack()
+            hero.take_damage(enemy_damage)
+
+    if hero.is_alive():
+        print(f"{hero.name} wins!")
+    else:
+        print(f"{enemy.name} wins!")
+
+
 
 
 def main():
     """Open the arena and introduce its first opponent."""
+
+
     print(f"Welcome to {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The Streets are opening...")
 
     goblin = Goblin("Richard")
+    
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
 
-    new_goblin = Goblin("jc")
-
-    print(f"{new_goblin.name} enters the arena with {new_goblin.health} health.")
+    hedo = Hero("Mitchell")
 
 
-    print("But no hero has answered the call... yet.")
+    battle(hedo,goblin)
+
 
 
 if __name__ == "__main__":
--- hero.py
+++ hero.py
@@ -1,4 +1,19 @@
-class Hero:
-    """The hero blueprint will be implemented later in the project."""
+import random
 
-    pass
+
+class Hero:
+    def __init__(self, name):
+      self.name = name
+      self.health = 100
+      self.attack_power = 20
+
+    def attack(self):
+        return random.randint(1,self.attack_power)
+
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+    
+    def is_alive(self):
+        return self.health > 0
